981a9da6bd7849806863e40608418c0f853fc7c9
pavel.prudky <Hogofogo83>
2021-02-03 13:46:28 +0100

    refactoring to main and app.go split

---
+++ main_test.go
@@ -0,0 +1,54 @@
+package main
+
+//https://github.com/kelvins/GoApiTutorial/blob/master/main_test.go
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+)
+
+var a App
+
+func TestMain(m *testing.M) {
+	a = App{}
+	a.Initialize("root", "", "rest_api_example")
+
+	//ensureTableExists()
+
+	code := m.Run()
+
+	//clearTable()
+
+	os.Exit(code)
+}
+
+func executeRequest(req *http.Request) *httptest.ResponseRecorder {
+	rr := httptest.NewRecorder()
+	a.Router.ServeHTTP(rr, req)
+
+	return rr
+}
+
+func checkResponseCode(t *testing.T, expected, actual int) {
+	if expected != actual {
+		t.Errorf("Expected response code %d. Got %d\n", expected, actual)
+	}
+}
+
+func TestGetNonExistentUser(t *testing.T) {
+	//clearTable()
+
+	req, _ := http.NewRequest("GET", "/user/45", nil)
+	response := executeRequest(req)
+
+	checkResponseCode(t, http.StatusNotFound, response.Code)
+
+	var m map[string]string
+	json.Unmarshal(response.Body.Bytes(), &m)
+	if m["error"] != "User not found" {
+		t.Errorf("Expected the 'error' key of the response to be set to 'User not found'. Got '%s'", m["error"])
+	}
+}
