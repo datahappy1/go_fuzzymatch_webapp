19c8c3661934985664ec6ad123f08c502f1dafb4
pavel.prudky <Hogofogo83>
2021-03-04 15:58:14 +0100

    cleaning up errors.js

--- api/client_examples/curl_sample.sh
+++ api/client_examples/curl_sample.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
+# POST REQUEST
+# *Nix terminal:
 curl --location --request POST 'http://localhost:8080/api/v1/requests/' \
---header 'Content-Type: application/json' \
+--header 'Content-Type: text/plain' \
 --data-raw '{
-    "stringsToMatch": "'\''apple, gmbh'\'','\''corp'\''",
-    "stringsToMatchIn": "hair, bear",
-    "mode": "combined"
+  "stringsToMatch": "'\''231 Beechwood Street'\'', '\''Helena, MT 59601'\'', '\''866 Brook Court'\'', '\''Harrison Township, MI 48045'\'', '\''40 Bayport Street'\'', '\''Virginia Beach, VA 23451'\'', '\''20 Hanover St.",
+	"stringsToMatchIn": "'\''231 Beechwood Street'\'', '\''Helena, MT 59601'\'', '\''866 Brook Court'\'', '\''Harrison Township, MI 48045'\'', '\''40 Bayport Street'\'', '\''Virginia Beach, VA 23451'\'', '\''20 Hanover St.'\''",
+	"mode": "combined"
 }'
 
+# Windows cmd ( https://stackoverflow.com/questions/11834238/curl-post-command-line-on-windows-restful-service ):
+curl -X POST -d "{""stringsToMatch"":""'31 Beechwood Street','Helena, MT 59601','866 Brook Court','Harrison Township, MI 48045','40 Bayport Street','Virginia Beach, VA 23451','20 Hanover St.'"",
+""stringsToMatchIn"":""'31 Beechwood Street','Helena, MT 59601','866 Brook Court','Harrison Township, MI 48045','40 Bayport Street','Virginia Beach, VA 23451','20 Hanover St.'"",
+""mode"":""combined""}" http://localhost:8080/api/v1/requests/
+
+
+# GET REQUEST
+# *Nix terminal:
 curl --location --request GET 'http://localhost:8080/api/v1/requests/4027cf00-6ff3-4239-9ec5-2d820b4f93cd/' \
 --data-raw ''
+
+# Windows cmd:
+curl -X GET http://localhost:8080/api/v1/requests/4027cf00-6ff3-4239-9ec5-2d820b4f93cd/
